cfe86c42104a90c6f913d25d206763615b8055f7
zhanglonglong <zhanglonglong@tbkt.cn>
2018-03-02 17:13:03 +0800

    1

--- Frame List/Vue/README.md
+++
@@ -1,6 +0,0 @@
-## Vue 官网：https://cn.vuejs.org/v2/guide/
-* Vue官网对Vue有详细的介绍，这里只记录需要注意的地方；
-    *
-        
-
-
--- Frame list/AngularJs2/项目案例/Heros/src/app/services/global.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class GlobalService {
-  serverUrl:string;
-  constructor() {
-    this.serverUrl='http://127.0.0.1:3000';
-  }
-}
